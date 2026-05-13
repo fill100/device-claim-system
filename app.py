8dbaec1dc0a9ecd0f21280f99bccf32b2c02cf1a
fill100 <78307583+fill100@users.noreply.github.com>
2026-05-13 12:18:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ with st.sidebar:
     st.markdown("# 💻 IT Management")
     st.page_link("app.py", label="Device Claim", icon="📑")
     st.page_link("pages/Wesgan.py", label="Asset System", icon="🛡️")
+    st.page_link("pages/Transfer.py", label="Asset System", icon="✈️")
     st.divider()
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
